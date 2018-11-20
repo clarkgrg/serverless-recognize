7edc9d8832002c74b17755e7bb9652da29f1bc71
Greg Clark <gclark@efcsystems.com>
2018-11-19 18:39:15 -0600

    Can process up to 5 tweets at a time

--- handler.js
+++ handler.js
@@ -16,14 +16,14 @@ const sendTweet = async (tweetText, questionTweetId) => {
   }
 }
 
-// Get a tweet sent to us
-const getTimeLineTweet = async (lastId) => {
+// Get a tweet sent to us.  Process at most 5 a time
+const getTimeLineTweets = async (lastId) => {
   try {
     let tweets = await T.get('statuses/mentions_timeline', 
-      { since_id: lastId, count: 1 });
+      { since_id: lastId, count: 5 });
 
     if (tweets && tweets.length > 0) {
-      return tweets[0];
+      return tweets;
     } else {
       return null;
     }
@@ -35,7 +35,7 @@ const getTimeLineTweet = async (lastId) => {
 
 // Get the Url of the image in the tweet
 const getImageUrl = (tweet) => {
-  if (tweet && tweet.entities && tweet.entities.media[0] && tweet.entities.media[0].media_url) {
+  if (tweet && tweet.entities && tweet.entities.media && tweet.entities.media[0] && tweet.entities.media[0].media_url) {
     return tweet.entities.media[0].media_url;
   } else {
     return null;
@@ -53,44 +53,56 @@ module.exports.recognize = async (event, context) => {
 
   // Get any new tweets
   let lastTweetId = await getLastId();
-  const tweet = await getTimeLineTweet(lastTweetId);  
+  const tweets = await getTimeLineTweets(lastTweetId);  
 
   let returnString = '';
+  let allResponseTweets = '';
 
   // Did anyone tweet us?
-  if (tweet && askImageQuestion(tweet.text)) {
-    try {
-      // save this tweet id so we only process once
-      const questionTweetId = tweet.id_str;
-      await saveLastId(questionTweetId);
+  if (tweets) {
+    for (let i = 0; i< tweets.length; i++) {
+      returnString = '';
+      let tweet = tweets[i];
+      if (askImageQuestion(tweet.text)) {
+        try {
+          // save this tweet id so we only process once
+          let questionTweetId = tweet.id_str;
+          await saveLastId(questionTweetId);
 
-      const sender = `@${tweet.user.screen_name}`;
+          let sender = `@${tweet.user.screen_name}`;
 
-      const imageUrl =  getImageUrl(tweet);
-      if (imageUrl) {
-        // Interpret the Image
-        returnString = await interpretImage(imageUrl); 
+          let imageUrl =  getImageUrl(tweet);
+          if (imageUrl) {
+            // Interpret the Image
+            returnString = await interpretImage(imageUrl); 
 
-        // An error occurred
-        if (returnString == null) {
-          returnString = "You stumped me!";
-        }     
-      } else {
-        returnString = "I don't see an image in the tweet";
+            // An error occurred
+            if (returnString == null) {
+              returnString = "You stumped me!";
+            }     
+          } else {
+            returnString = "I don't see an image in the tweet";
+          }
+
+          // Send a response tweet
+          let responseTweet = `${sender} ${returnString}`;
+          let answerTweet = await sendTweet(responseTweet, questionTweetId);  
+
+          // Write responseTweet for monitoring
+          console.log(responseTweet);
+
+          allResponseTweets = allResponseTweets + responseTweet + ', ';
+        } catch (err) {
+          console.log(err);
+        }
       }
-
-      // Send a response tweet
-      const responseTweet = `${sender} ${returnString}`;
-      const answerTweet = await sendTweet(responseTweet, questionTweetId);  
-    } catch (err) {
-      console.log(err);
     }
   }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: returnString,
+      message: allResponseTweets,
     }),
   };
 };
